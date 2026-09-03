adb3601558d6cb416f755e6aac1ea3b7a6e9e081
ted111712 <ted111712@gmail.com>
2026-09-03 19:23:39 +0900

    feat(crud): todo 레포지토리 구현

---
+++ app/repositories/todos.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models.todos import Todo
+
+
+class TodoRepository:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def get_all(self) -> list[Todo]:
+        stmt = select(Todo)
+        return list(self.session.scalars(stmt).all())
+
+    def get_by_id(self, todo_id: int) -> Todo | None:
+        return self.session.get(Todo, todo_id)
+
+    def create(self, todo: Todo) -> Todo:
+        self.session.add(todo)
+        self.session.commit()
+        self.session.refresh(todo)
+        return todo
+
+    def delete(self, todo: Todo) -> None:
+        self.session.delete(todo)
+        self.session.commit()
