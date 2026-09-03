1608004dd48057d0e104dab93a4e3c8ef38d897d
ted111712 <ted111712@gmail.com>
2026-09-03 18:59:34 +0900

    feat(crud): Todo 모델 정의

---
+++ app/models/todos.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    is_completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    )
